4b6d704235758f017627da10e3137909076711e3
PhazeIllusions <Guest@WTS5.local>
2015-06-28 19:03:22 -0700

    Money

---
+++ IntroToJava/src/CanHeRide.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+
+public class CanHeRide
+{
+public static void main(String[] args)
+{
+	String height = JOptionPane.showInputDialog(null, "What is your height in inches?");
+	int heightinch = Integer.parseInt(height);
+	if(heightinch >= 50) {
+		JOptionPane.showMessageDialog(null, "You may enter the ride.");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "GTFO");
+	}
+}
+}
---
+++ IntroToJava/src/SoundtrackToLife.java
@@ -0,0 +1,43 @@
+import java.net.URI;
+
+import javax.swing.JOptionPane;
+
+public class SoundtrackToLife {
+
+	public static void main(String[] args) {
+
+		// 1. Adjust this pop-up to find out what mood the user is in.
+		int userMood = JOptionPane.showOptionDialog(null, "What is your Mood?", "Mood Detector", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Stressed", "Sad", "Angry" }, null);
+		
+		// 2. Their answer is stored in the userMood variable. Print it out.
+System.out.println(userMood);
+		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
+if (userMood == 0)
+{
+	playVideo(www.youtube.com/watch?v=dzwCZkORDHg);
+} else if (userMood == 1)
+{
+	playVideo(www.youtube.com/watch?v=ZbZSe6N_BXs);
+}else
+{
+	playVideo(www.youtube.com/watch?v=UXgqDlrqmzo);
+}
+		// 4. Play different songs for other moods.
+
+		// If you are seeing ads at the beginning of your videos, install Adblock.
+
+	}
+
+	static void playVideo(String youTubeLink) {
+		try {
+			URI uri = new URI(youTubeLink);
+			java.awt.Desktop.getDesktop().browse(uri);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
